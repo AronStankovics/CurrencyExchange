761c3739765f7548723470e58382b0d0cc3e887e
stankovics.aron <stankovics.aron@gmail.com>
2016-12-12 20:17:27 +0100

    clean up

--- CurrencyExchange/App_Start/BundleConfig.cs
+++ CurrencyExchange/App_Start/BundleConfig.cs
@@ -37,9 +37,6 @@ namespace CurrencyExchange
             bundles.Add(new ScriptBundle("~/bundles/rickshaw").Include(
                        "~/Scripts/d3.v3.min.js",
                        "~/Scripts/rickshaw.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/rickshawcontroller").Include(
-                        "~/Scripts/Controller/rickshawcontroller.js"));
         }
     }
 }
